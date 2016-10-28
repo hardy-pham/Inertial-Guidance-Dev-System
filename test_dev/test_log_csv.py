bc8563ce4da51d45cfabb8e731795e1b350af80d
Hardy <h6pham8@gmail.com>
2016-10-27 23:03:07 -0700

    implemented unique time as part of csv file creation

--- test_dev/test_log_csv.py
+++ test_dev/test_log_csv.py
@@ -1,10 +1,15 @@
 # test for .csv functionality
-
 import csv
+import time
+
 row = [1,2]
 test = 111
 
-with open(str(test)+'templog.csv', 'a') as f:
+# epoch time
+epoch = int(time.time())
+
+# unique .csv file on every run
+with open(str(epoch) + '_log.csv', 'a') as f:
     w = csv.writer(f)
     w.writerow(row)
     w.writerow(['hello'])
